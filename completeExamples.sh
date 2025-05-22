31de5e7e54a2ba79c8b3a291ed1a132a1e2daff2
Michael Schifferer <misc00021@stud.uni-saarland.de>
2025-05-22 12:16:46 +0200

    Results

--- completeExamples.sh
+++ completeExamples.sh
@@ -1,41 +1,51 @@
 #!/bin/bash
 
-#limited by rules
+# Limited by rules
 for file in Examples/MyExamples/*.p; do
-        for i in 100 500 1000 50000; do
-            for j in 1 10; do
-                start=$(date +%s.%N)
-                twee --complete --max-rules $i --random-mode-best-of $j "$file" > "${file%.*}${i}RulesBestOf${j}";
-                end=$(date +%s.%N)
-                echo -e "\nExecution time: $(awk "BEGIN {print $end - $start}") seconds\n" >> "${file%.*}${i}RulesBestOf${j}";
-                done
-            done
-        done
+    base=$(basename "$file")
+    name="${base%.*}"
+    mkdir -p "Examples/MyExamples/Results/$name"
+    output_file="Examples/MyExamples/Results/$name/$name"
 
-#limited by term size
-for file in Examples/MyExamples/*.p; do
-        for i in 3 5 8; do
+    for i in 100 500 1000 5000; do
+        for j in 1 10; do
+            if [[ $j == 10 && ( $i == 1000 || $i == 5000 ) ]]; then
+                continue
+            fi
             start=$(date +%s.%N)
-            twee --complete --max-term-size $i "$file" > "${file%.*}${i}TermSize";
+            if ! timeout 10800s twee --complete --max-rules "$i" --random-mode-best-of "$j" "$file" > "${output_file}${i}RulesBestOf${j}" ; then
+                echo -e "\nTimeout after 3 hours\n" >> "${output_file}${i}RulesBestOf${j}"
+            fi
             end=$(date +%s.%N)
-            echo -e "\nExecution time: $(awk "BEGIN {print $end - $start}") seconds\n" >> "${file%.*}${i}TermSize";
-            done
+            echo -e "\nExecution time: $(awk "BEGIN {print $end - $start}") seconds\n" >> "${output_file}${i}RulesBestOf${j}"
         done
+    done
 
-#limited by CP depth
-for file in Examples/MyExamples/*.p; do
-        for i in 3 5 8; do
-            start=$(date +%s.%N)
-            twee --complete --max-cp-depth $i "$file" > "${file%.*}${i}CPDepth";
-            end=$(date +%s.%N)
-            echo -e "\nExecution time: $(awk "BEGIN {print $end - $start}") seconds\n" >> "${file%.*}${i}CPDepth";
-            done
-        done
-
-#complete subsets
-for file in Examples/MyExamples/*.p; do
+    # Limited by term size
+    for i in 3 5 8; do
         start=$(date +%s.%N)
-        twee --complete --max-rules 500 --complete-subsets "$file" > "${file%.*}CompleteSubsets";
+        if ! timeout 10800s twee --complete --max-term-size "$i" "$file" > "${output_file}${i}TermSize" ; then
+            echo -e "\nTimeout after 3 hours\n" >> "${output_file}${i}TermSize"
+        fi
         end=$(date +%s.%N)
-        echo -e "\nExecution time: $(awk "BEGIN {print $end - $start}") seconds\n" >> "${file%.*}CompleteSubsets";
-        done
+        echo -e "\nExecution time: $(awk "BEGIN {print $end - $start}") seconds\n" >> "${output_file}${i}TermSize"
+    done
+
+    # Limited by CP depth
+    for i in 1 2 3; do
+        start=$(date +%s.%N)
+        if ! timeout 10800s twee --complete --max-cp-depth "$i" "$file" > "${output_file}${i}CPDepth"; then
+            echo -e "\nTimeout after 3 hours\n" >> "${output_file}${i}CPDepth"
+        fi
+        end=$(date +%s.%N)
+        echo -e "\nExecution time: $(awk "BEGIN {print $end - $start}") seconds\n" >> "${output_file}${i}CPDepth"
+    done
+
+    # Complete subsets
+    start=$(date +%s.%N)
+    if ! timeout 10800s twee --complete --max-rules 500 --complete-subsets "$file" > "${output_file}CompleteSubsets"; then
+        echo -e "\nTimeout after 3 hours\n" >> "${output_file}CompleteSubsets"
+    fi
+    end=$(date +%s.%N)
+    echo -e "\nExecution time: $(awk "BEGIN {print $end - $start}") seconds\n" >> "${output_file}CompleteSubsets"
+done
