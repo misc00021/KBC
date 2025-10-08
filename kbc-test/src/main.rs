7ca260f113136c47aec37d88d233f6613101adba
Michael Schifferer <misc00021@stud.uni-saarland.de>
2025-10-08 13:50:15 +0200

    Test run

--- kbc-test/src/main.rs
+++ kbc-test/src/main.rs
@@ -162,10 +162,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         ),
         ("replacing_no_unorderable", kbc_replacing_no_unorderable()),
         ("replacing_sep_div", kbc_replacing_sep_div()),
-        (
+        /*(
             "replacing_sep_div_no_unorderable",
             kbc_replacing_sep_div_no_unorderable(),
-        ),
+        ),*/
     ];
     for time_limit in vec![
         0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1., /*, 2., 3., 4., 5.*/
